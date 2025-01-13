d07c6a1375490065bc6eb015f2bbf4e1580e37e6
KQ <eternal.september@gmail.com>
2025-01-12 21:31:05 -0500

    number of posts in a category is sortable in the admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,7 @@ from blog.models import Category, Post
 from markdownx.admin import MarkdownxModelAdmin
 from django.urls import reverse
 from django.utils.html import format_html
+from django.db.models import Count
 
 
 # These classes are for customizing the appearance of models in the admin panel.
@@ -10,13 +11,22 @@ from django.utils.html import format_html
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     ordering = ['name']
-    list_display = ('name', 'get_num_posts')
+    list_display = ('name', 'post_count')
     readonly_fields = ['get_posts']
 
-    @admin.display(description="number of posts")
-    def get_num_posts(self, obj):
-        return len(obj.posts.all())
+    # Gets the number of posts in a category.
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.annotate(post_count=Count("posts"))
 
+    @admin.display(description="number of posts")
+    def post_count(self, obj):
+        return obj.post_count
+
+    post_count.admin_order_field = 'post_count'
+    
+
+    # Generates a list of links to posts under a category.
     @admin.display(description='posts')
     def get_posts(self, obj):
         return self.links_to_posts(obj.posts.all().order_by("-created_on"))
