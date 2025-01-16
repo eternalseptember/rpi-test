cac812f16f6238bf1671074ea7b53df80cd752b4
KQ <eternal.september@gmail.com>
2025-01-16 16:45:27 -0500

    the cleanest version of inheriting an object that prints a calendar

--- blog/custom_calendar.py
+++ blog/custom_calendar.py
@@ -2,11 +2,8 @@ from calendar import HTMLCalendar
 
 class BlogHTMLCalendar(HTMLCalendar):
 
-    def __init__(self, year):
-        self.year = year
-        return super(BlogHTMLCalendar, self).__init__()
-    
+    def formatyear(self, year):
+        return super().formatyear(year)
 
-    def formatyear(self):
-        return super().formatyear(self.year)
+
 
