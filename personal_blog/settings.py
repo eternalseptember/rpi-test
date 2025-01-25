638f81130afd097a64bc5748c17e447cf3afaf02
KQ <eternal.september@gmail.com>
2025-01-25 18:41:36 -0500

    minor space changes

--- personal_blog/settings.py
+++ personal_blog/settings.py
@@ -18,9 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
 from decouple import config, Csv
-
 SECRET_KEY = config("SECRET_KEY")
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
